e1aac1e553af9d79640c08e62450c4380a0caf8d
mckeea <Dudek.Maciej@eea.europa.eu>
2025-09-16 13:29:11 +0000

    Config updates

--- .github/scripts/build-docs.sh
+++ .github/scripts/build-docs.sh
@@ -4,8 +4,6 @@ set -e
 echo "🔄 Copying DOCS to origin_DOCS..."
 mv DOCS origin_DOCS
 
-# Install PyYAML if not already installed (TODO: move it to docker image)
-python3 -m pip install --break-system-packages PyYAML
 
 echo "🔄 Updating URL mappings..."
 python3 .github/scripts/update_url_mappings.py
